970fc2831dd3c6f3147281c702695b279c1f67cf
Yen Han Sern <hansern.yen@ifastfinancial.com>
2019-05-30 10:15:56 +0800

    Initial commit

---
+++ src/app/auth.service.ts
@@ -0,0 +1,26 @@
+export class AuthService {
+  loggedIn = false;
+/*
+*
+Fake login to use auth guard
+*
+*/
+  isAuthenticated() {
+    const promise = new Promise(
+      (resolve, reject) => {
+        setTimeout(() => {
+          resolve(this.loggedIn);
+        }, 800);
+      }
+    );
+    return promise;
+  }
+
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+}
